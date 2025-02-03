56f19baf0bac426c384c12bfeb15cd1ab76446c7
fowosere ademola <fowosereademola@gmail.com>
2025-02-03 21:15:44 +0000

    root urlfunctionality

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,8 @@ app.use(cors());
 
 app.use(numberRoutes);
 
-// app.get("/", (req, res) => {
-//   res.status(200).json({ message: "Welcome to my API!" });
-// });
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to my API!" });
+});
 
 export default app;
